db2ce7e5f96313b802c410c9ffe612a65b7730e9
leonardo.mendes@4linux.com.br <claudiasoliveira00@gmail.com>
2018-06-27 22:06:17 -0300

    modificado

--- lista.py
+++ lista.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-num = int(input('Digite um número: '))
+# num = int(input('Digite um número: '))
 
-for x in range(0, 100,2):
-    print(x)
+# for x in range(0, 100,2):
+#     print(x)
 
 
 # for x in range(num):
@@ -16,15 +16,14 @@ for x in range(0, 100,2):
 
 
 # nomes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# range cria os número até o valor especificado ou intervalo de valores
+# # range cria os número até o valor especificado ou intervalo de valores
 # numeros = list(range(40, 100))
 # print(numeros)
 
-#if 20 in numeros:
+# if 20 in numeros:
 #    print('sim')
-#else:
+# else:
 #    print('não')
-
 
 # nomes = ['claudia','maria','joao', 'jose']
 # numeros = list(range(40, 100))
@@ -58,4 +57,56 @@ for x in range(0, 100,2):
 # nomes.remove('daniel')
 
 #print(nomes)
+
+# index mostra a posição requerida
+# nomes = ['claudia','maria','joao', 'jose']
+
+# # nomes.reverse()
+# nomes.index('joao')
+# nomes[3]
+# print(nomes)
+
+# função len verifica o tamanho da lista
+
+# metodo set não exibe os valores duplicados
+# nomes = ['claudia','maria','joao', 'jose']
+# nomes.insert(0, 'daniel')
+# print(nomes)
+# set(nomes)
+# print(list(set(nomes)))
+
+# método sort ordena em ordem alfabética
+# nomes = ['claudia','maria','joao', 'jose']
+# nomes.reverse()
+# print(nomes)
+# nomes.sort())
+# print(nomes)
+
+
+# método count 
+
+# nomes = ['claudia','maria','joao', 'jose']
+# nomes.count('daniel')
+# print(nomes('joao')
+
+#método ord mostra a posição da letra internamente
+# ord('J')
+# método chr mostra qual letra é o valor inserido
+# chr(74)
+# 97 é a letra a 
+
+
+# or x in range(97, 97  +27):
+# 97 a posição inicial, 97 é o parametro + 27 
+# for x in range(97, 97  +27):
+# ... print(chr(x))
+
+# letras= []
+# for x in range(97, 97+26):
+#     letras.append(chr(x))
+# print(letras)
+
+# metodo split: para criar uma lista
+# nome = 'claudia silva de oliveira'
+# nome.split()
 
